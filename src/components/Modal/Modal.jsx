d4605c05b27fadb8a4bd5a4707a871a0e55ac9eb
Evgenij Prishchepov <prishchepovee@gmail.com>
2023-09-17 13:31:32 +0300

    deploy

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -15,7 +15,7 @@ class Modal extends Component {
     }
   };
   render() {
-    const { largeImageURL, onClick, onKeyDown } = this.props;
+    const { largeImageURL, onClick } = this.props;
     return (
       <div className={css.Overlay} onClick={onClick}>
         <div className={css.Modal}>
